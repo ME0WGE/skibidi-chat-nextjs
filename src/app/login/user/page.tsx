c5f2136724456487cc12fb4c019732c6e7863301
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-25 10:15:47 +0200

    add login page

--- src/app/login/user/page.tsx
+++ src/app/login/user/page.tsx
@@ -1,9 +1,14 @@
 export default function Page() {
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <h1 className="text-4xl">Welcome to SkiBiDi Chatge!</h1>
+      <p className="italic">
+        Feel free to{" "}
+        <span className="text-green-400 cursor-pointer">create an account</span>{" "}
+        <br />
+        or <br /> to join the chat as a{" "}
+        <span className="text-blue-400 cursor-pointer">Guest</span>!
+      </p>
+    </div>
   );
 }
