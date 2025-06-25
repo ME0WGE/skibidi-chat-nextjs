2db4e5ffda23a5e18b3c99dbef98fa9d985c6637
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-25 10:23:22 +0200

    add disclaimer

--- src/app/login/user/page.tsx
+++ src/app/login/user/page.tsx
@@ -1,13 +1,18 @@
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+    <div className="flex-1 flex flex-col items-center justify-center text-center">
       <h1 className="text-4xl">Welcome to SkiBiDi Chatge!</h1>
       <p className="italic">
         Feel free to{" "}
-        <span className="text-green-400 cursor-pointer">create an account</span>{" "}
+        <span className="text-green-400 cursor-pointer">create an account</span>
         <br />
         or <br /> to join the chat as a{" "}
         <span className="text-blue-400 cursor-pointer">Guest</span>!
+      </p>
+      <p className="mt-6 text-sm text-gray-400 max-w-md">
+        Disclaimer: This is a passion project developed to learn Next.js.
+        <br />
+        s/o <span className="cursor-pointer text-xl">MolenGeek</span>
       </p>
     </div>
   );
